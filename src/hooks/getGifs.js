6a3aef519d0db31ace50c5cf0cf8075b7dc75a06
fiorafran <francofioravanti21@gmail.com>
2022-06-27 20:25:46 -0300

    remove apky

--- src/hooks/getGifs.js
+++ src/hooks/getGifs.js
@@ -1,6 +1,6 @@
 export const getGifs = async(category) => {
     const parseCategory = category.replace(/\s/g, '+');
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=OBRjMT5IxKXzteX4ohkxC1NlH3uI4PdI&q=${parseCategory}&limit=15&offset=0&rating=g&lang=es`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.REACT_APP_API_KEY}&q=${parseCategory}&limit=15&offset=0&rating=g&lang=es`;
     const res = await fetch(url);
     const { data } = await res.json();
 
